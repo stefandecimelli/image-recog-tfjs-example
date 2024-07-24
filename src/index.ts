f8b3c98dead574157c047fbde9e62f8be552d787
Stefan <decimelli@ibm.com>
2024-07-24 12:57:47 -0400

    add new images

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,9 @@ const cocoSsd = require('@tensorflow-models/coco-ssd');
 const tf = require('@tensorflow/tfjs-node');
 const fs = require('fs').promises;
 
-Promise.all([cocoSsd.load(), fs.readFile('assets/image1.jpg')]).then((results) => {
+const imageToTest = "assets/image2.jpg";
+
+Promise.all([cocoSsd.load(), fs.readFile(imageToTest)]).then((results) => {
     const model = results[0];
     const imgTensor = tf.node.decodeImage(new Uint8Array(results[1]), 3);
     return model.detect(imgTensor);
