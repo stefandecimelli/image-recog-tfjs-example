788f436d44fc59123726927bcb780fa6c5fed534
Stefan <decimelli@ibm.com>
2024-07-24 13:06:08 -0400

    swithc to module imports

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-const cocoSsd = require('@tensorflow-models/coco-ssd');
-const tf = require('@tensorflow/tfjs-node');
-const fs = require('fs').promises;
+import * as cocoSsd from "@tensorflow-models/coco-ssd";
+import * as tf from "@tensorflow/tfjs-node";
+import fs from "fs/promises";
 
-const imageToTest = "assets/image2.jpg";
+const imageToTest = "assets/image3.jpg";
 
 Promise.all([cocoSsd.load(), fs.readFile(imageToTest)]).then((results) => {
     const model = results[0];
     const imgTensor = tf.node.decodeImage(new Uint8Array(results[1]), 3);
-    return model.detect(imgTensor);
+    return model.detect(imgTensor as tf.Tensor3D);
 }).then((predictions) => {
     console.log(JSON.stringify(predictions, null, 2));
 });
